66cb4184354af854bec84a92e3d1bd1e22786726
Anthony <a.a.donovan@outlook.com>
2020-05-18 16:32:45 +0100

    Adds top_up method to Oystercard class, adds associated rspec tests

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,10 +1,21 @@
 require 'oyster_card'
 describe Oystercard do
-  it { is_expected.to be_instance_of Oystercard}
+  describe '#balance' do
+    it { is_expected.to be_instance_of Oystercard}
 
-  it { is_expected.to respond_to :balance }
+    it { is_expected.to respond_to :balance }
 
-  it "return starting balance of 0" do
-    expect(subject.balance).to eq 0
+    it "return starting balance of 0" do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'tops up card balance using passed value argument' do
+      subject.top_up(100)
+      expect(subject.balance).to eq 100
+    end
   end
 end
