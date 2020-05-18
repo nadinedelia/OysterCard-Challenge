03dfc68c12c25054e73e0530e4e1567883823949
minsi yang <yangminsi@gmail.com>
2020-05-18 16:13:45 +0100

    Created test for balance method, passed. Added initialize method to use @balance, attr_reader for @balance

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,3 +1,10 @@
+require 'oyster_card'
 describe Oystercard do
-  
+  it { is_expected.to be_instance_of Oystercard}
+
+  it { is_expected.to respond_to :balance }
+
+  it "return starting balance of 0" do
+    expect(subject.balance).to eq 0
+  end
 end
