c2aac6aafcf1b3f3464b160f87fa7f53aa402724
nadine <nadine.loepfe@gmail.com>
2020-05-19 17:05:00 +0100

    initialized station, passing tests

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -5,4 +5,19 @@ require 'station'
       it "takes one argument" do
         expect(Station).to respond_to(:new).with(2).argument
       end
+
+    it 'responds to name' do
+      expect(Station).to respond_to(:name)
     end
+
+    it 'exposes the name' do
+      subject = Station.new('Homerton', 'Zone 2')
+      expect(subject.name).to eq 'Homerton'
+    end
+
+    it 'exposes the zone' do
+      subject = Station.new('Homerton', 'Zone 2')
+      expect(subject.zone).to eq 'Zone 2'
+    end
+
+  end
